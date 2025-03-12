610fc3e0db3d2c673814ee2245aa3a7c29e28137
Thierry56511 <thierry.dechene@usherbrooke.ca>
2025-03-12 19:04:54 -0400

    Update classical_shortest_path_finder.py

--- scripts/classical_shortest_path_finder.py
+++ scripts/classical_shortest_path_finder.py
@@ -13,6 +13,7 @@ from my_research.utils.grid_dijkstra import generer_grille
 def _build_arg_parser():
     p = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
+    p.add_argument('--size', type=int, default=10, help="size of the grid (use a int)")
     p.add_argument('--obstacle_mode', choices=['ratio', 'number'], default='ratio', help="Obstacle mode selection ('ratio' or 'number')")
     p.add_argument('--obstacle_ratio', type=float, default=0.2, help="Obstacle ratio (use if 'obstacle_mode' is 'ratio')")
     p.add_argument('--obstacle_number', type=int, default=20, help="Number of obstacles (use if 'obstacle_mode' is 'number')")
