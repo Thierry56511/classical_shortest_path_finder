ff9297933756fa0b4a87a6c0786cf946b4ae41ea
Thierry56511 <thierry.dechene@usherbrooke.ca>
2025-03-18 11:28:05 -0400

    Update classical_shortest_path_finder.py

--- scripts/classical_shortest_path_finder.py
+++ scripts/classical_shortest_path_finder.py
@@ -12,10 +12,8 @@ from my_research.utils.grid_dijkstra import (dijkstra_stepwise, astar_stepwise, 
 def _build_arg_parser():
     p = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
-    p.add_argument('--size', type=int, default=10, help="size of the grid (use a int)")
-    p.add_argument('--obstacle_mode', choices=['ratio', 'number'], default='ratio', help="Obstacle mode selection ('ratio' or 'number')")
-    p.add_argument('--obstacle_ratio', type=float, default=0.2, help="Obstacle ratio (use if 'obstacle_mode' is 'ratio')")
-    p.add_argument('--obstacle_number', type=int, default=20, help="Number of obstacles (use if 'obstacle_mode' is 'number')")
+    p.add_argument("--input", type=str, required=True, help="Name of the file JSON containing the graph")
+    p.add_argument("--shortestpath", type=str, required=True, help="Name of the file JSON containing the graph")
     return p
 
 def main():
