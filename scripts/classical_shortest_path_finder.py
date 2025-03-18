c7b9265e283d5c8fca72a459c945205d44089104
Thierry56511 <thierry.dechene@usherbrooke.ca>
2025-03-18 11:41:40 -0400

    Update classical_shortest_path_finder.py

--- scripts/classical_shortest_path_finder.py
+++ scripts/classical_shortest_path_finder.py
@@ -7,20 +7,22 @@ Find the shortest path between 2 points in a graph with Dijkstra or A*.
 
 import argparse
 import os
-from my_research.utils.grid_dijkstra import (dijkstra_stepwise, astar_stepwise, heuristic)
+from my_research.utils.grid_dijkstra import (dijkstra_stepwise, astar_stepwise, heuristic, load_graph)
 
 def _build_arg_parser():
     p = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
     p.add_argument("--input", type=str, required=True, help="Name of the file JSON containing the graph")
     p.add_argument("--shortestpath", choices=['Dijkstra', 'A*'], default='Dijkstra', help="shortest path algorithm selection ('Dijkstra' or 'A*')")
+    p.add_argument("--output", type=str, required=True, help="Name of the file (ex: graph.json)")
     return p
 
 def main():
     parser = _build_arg_parser()
     args = parser.parse_args()
 
-    grid, G = generer_grille(args.size, args.obstacle_mode, args.obstacle_ratio, args.obstacle_number)
+    
+    G = load_graph(args.size, args.obstacle_mode, args.obstacle_ratio, args.obstacle_number)
     save_graphe(G, args.output)
     # Afficher la grille générée et les noeuds du graph
     print("Grille générée :")
