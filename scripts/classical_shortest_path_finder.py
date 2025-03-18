cd364ab1f350c73410a68cee41ca72cdbd2db197
Thierry56511 <thierry.dechene@usherbrooke.ca>
2025-03-18 12:52:53 -0400

    Update classical_shortest_path_finder.py

--- scripts/classical_shortest_path_finder.py
+++ scripts/classical_shortest_path_finder.py
@@ -7,14 +7,16 @@ Find the shortest path between 2 points in a graph with Dijkstra or A*.
 
 import argparse
 import os
-from my_research.utils.grid_dijkstra import (dijkstra_stepwise, astar_stepwise, heuristic, load_graph)
+from my_research.utils.grid_dijkstra import (dijkstra_stepwise, astar_stepwise, heuristic, get_neighbors_diagonal, load_graph)
 
 def _build_arg_parser():
     p = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
-    p.add_argument("--input", type=str, required=True, help="Name of the file JSON containing the graph")
+    p.add_argument("--input", type=str, required=True, help="Name of the file JSON containing the graph (ex: graph.json")
     p.add_argument("--shortestpath", choices=['Dijkstra', 'A*'], default='Dijkstra', help="shortest path algorithm selection ('Dijkstra' or 'A*')")
-    p.add_argument("--output", type=str, required=True, help="Name of the file (ex: graph.json)")
+    p.add_argument("--start", type=str, required=True, help="starting node (ex: '3,4')") 
+    p.add_argument("--target", type=str, required=True, help="ending node (ex: '7,8')")
+    p.add_argument("--diagonal_mode", choices=['diagonal', 'nondiagonal'], default='nondiagonal', help="Choose if the algorithm can travel in diagonal or not ('diagonal' or 'nondiagonal')")
     return p
 
 def main():
@@ -23,11 +25,14 @@ def main():
 
     
     G = load_graph(args.input)
-    save_graphe(G, args.output)
-    # Afficher la grille générée et les noeuds du graph
-    print("Grille générée :")
-    print(grid)
-    print("Graph nodes:", list(G.nodes))
+    
+    if args.shortestpath == "Dijkstra":
+        shortest_path = dijkstra_stepwise(G, args.start, args.target, args.diagonal_mode)
+    elif args.shortestpath == "A*":
+        shortest_path = astar_stepwise(G, args.start, args.target, args.diagonal_mode)
+    
+    print("Shortest path :")
+    print(shortest_path)
 
 if __name__ == "__main__":
     main()
