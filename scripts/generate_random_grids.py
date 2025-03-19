ab5627f67aa9497f72399e891d16f44b7c43e11e
Thierry56511 <thierry.dechene@usherbrooke.ca>
2025-03-19 14:02:40 -0400

    Update generate_random_grids.py

--- scripts/generate_random_grids.py
+++ scripts/generate_random_grids.py
@@ -15,7 +15,7 @@ def _build_arg_parser():
     p.add_argument('--size', type=int, default=10, help="size of the grid (use a int)")
     p.add_argument('--obstacles', type=str, default='ratio:0.2', 
                help="Obstacle settings: 'ratio:<value>' or 'number:<value>'")
-    p.add_argument("--output", type=str, required=True, help="Name of the file (ex: graph.json)")
+    p.add_argument("--output", type=str, required=True, help="Name of the file and number of graphs saved: 'graph.json;<number>'")
     return p
 
 def main():
@@ -24,12 +24,16 @@ def main():
 
     mode, value = args.obstacles.split(':')
     value = float(value) if mode == 'ratio' else int(value)
-
+    
     if mode == 'ratio' and value > 1:
         raise IOError('The obstacle ratio should not higher than 1.0'.format(args.obstacles))
 
+    file, number = args.obstacles.split(';')
+    number = int(number)
+    file = str(file)
+
     grid, G = generer_grille(args.size, mode, value, value)
-    save_graph(G, args.output)
+    save_graph(G, file, number)
     print(f"✅ Graphe sauvegardé dans '{args.output}'.")
     # Afficher la grille générée et les noeuds du graph
     print("Grille générée :")
