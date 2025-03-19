2a932197d3543651f39a90d30d29571d3bd7e8f0
Thierry56511 <thierry.dechene@usherbrooke.ca>
2025-03-19 12:22:29 -0400

    Update generate_random_grids.py
    input optimization

--- scripts/generate_random_grids.py
+++ scripts/generate_random_grids.py
@@ -13,9 +13,8 @@ def _build_arg_parser():
     p = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
     p.add_argument('--size', type=int, default=10, help="size of the grid (use a int)")
-    p.add_argument('--obstacle_mode', choices=['ratio', 'number'], default='ratio', help="Obstacle mode selection ('ratio' or 'number')")
-    p.add_argument('--obstacle_ratio', type=float, default=0.2, help="Obstacle ratio (use if 'obstacle_mode' is 'ratio')")
-    p.add_argument('--obstacle_number', type=int, default=20, help="Number of obstacles (use if 'obstacle_mode' is 'number')")
+    p.add_argument('--obstacles', type=str, default='ratio:0.2', 
+               help="Obstacle settings: 'ratio:<value>' or 'number:<value>'")
     p.add_argument("--output", type=str, required=True, help="Name of the file (ex: graph.json)")
     return p
 
@@ -23,7 +22,10 @@ def main():
     parser = _build_arg_parser()
     args = parser.parse_args()
 
-    grid, G = generer_grille(args.size, args.obstacle_mode, args.obstacle_ratio, args.obstacle_number)
+    mode, value = args.obstacles.split(':')
+    value = float(value) if mode == 'ratio' else int(value)
+
+    grid, G = generer_grille(args.size, mode, value, value)
     save_graph(G, args.output)
     print(f"✅ Graphe sauvegardé dans '{args.output}'.")
     # Afficher la grille générée et les noeuds du graph
